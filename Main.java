93677917fd10f5ceb294a05993eb53c2a5ebb5c8
xersky <ayoub.elaoufi@gmail.com>
2023-12-20 17:31:03 +0100

    writing instructions for a summation algorithm (from 1 to 23) in bytecode using our vm opcodes

--- Main.java
+++ Main.java
@@ -2,8 +2,58 @@ public class Main {
     public static void main(String[] args) {
         VirtualMachine vm = new VirtualMachine();
 
-        byte[] testByteArray = {00,00,00,00,23,00,00,00,00,07,0x0E,00,00,0x0F,00,02,0x08};
+        byte[] summationOfTwentyThreeByteArray = {
+            //push 23
+            00,00,00,00,0x17,
+            //push 1
+            00,00,00,00,01,
+            //store 00 01
+            0x0D,00,01,
+            //store 00 00
+            0x0D,00,00,
+            //push 0
+            00,00,00,00,00,
+            //push 1
+            00,00,00,00,01,
+            //store 00 03
+            0x0D,00,03,
+            //store 00 02
+            0x0D,00,02,
+            //load 00 02 -starting the loop
+            0x0C,00,02,
+            //load 00 03
+            0x0C,00,03,
+            //add
+            02,
+            //store 00 02
+            0x0D,00,02,
+            //load 00 03
+            0x0C,00,03,
+            //push 1
+            00,00,00,00,01,
+            //add
+            02,
+            //store 00 03
+            0x0D,00,03,
+            //load 00 03
+            0x0C,00,03,
+            //load 00 00,
+            0x0C,00,00,
+            //eq
+            0x12,
+            //cjump - conditional jump to loop
+            0x0B,(byte) 0xFF,(byte) 0xE0,
+            //load 00 02
+            0x0C,00,02,
+            //load 00 00
+            0x0C,00,00,
+            //add - last argument
+            02,
+            //return
+            0x08 
 
-        System.out.println(vm.byteInterpreter(testByteArray));
+        };
+
+        System.out.println(vm.byteInterpreter(summationOfTwentyThreeByteArray));
     }
 }
