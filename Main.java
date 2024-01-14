d3adf37a00740e987503deaf5b741c4e796675d1
xersky <ayoub.elaoufi@gmail.com>
2024-01-14 22:35:11 +0100

    fix main

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         VirtualMachine vm = new VirtualMachine();
 
         byte[] summationOfTwentyThreeByteArray = {
